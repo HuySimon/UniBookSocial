e37b689d9e7ab420624531b93ca4137118b0ef08
AnhHuy <anhhuy2452003@gmail.com>
2023-10-01 23:04:42 +0700

    Users_UI

--- client/src/views/pages/Users/index.jsx
+++ client/src/views/pages/Users/index.jsx
@@ -70,8 +70,11 @@ const Users = () => {
             </tr>
         </thead>
         <tbody>
+            
             <tr className="bg-white border-b -800 -700 hover:bg-gray-50 -gray-600">
-                
+                <td className="px-6 py-4">
+                    1
+                </td>
                 <th scope="row" className="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap ">
                     <img className="w-10 h-10 rounded-full" src="/docs/images/people/profile-picture-1.jpg" alt="Jese image" />
                     <div className="pl-3">
@@ -80,7 +83,13 @@ const Users = () => {
                     </div>  
                 </th>
                 <td className="px-6 py-4">
-                    React Developer
+                    Neil
+                </td>
+                <td className="px-6 py-4">
+                    Sims
+                </td>
+                <td className="px-6 py-4">
+                    Admin
                 </td>
                 <td className="px-6 py-4">
                     <div className="flex items-center">
@@ -88,20 +97,29 @@ const Users = () => {
                     </div>
                 </td>
                 <td className="px-6 py-4">
-                    <a href="#" className="font-medium text-blue-600 -500 hover:underline">Edit user</a>
+                    {/* <!-- Modal toggle --> */}
+                    <a href="#" type="button" data-modal-target="editUserModal" data-modal-show="editUserModal" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit user</a>
                 </td>
             </tr>
             <tr className="bg-white border-b -800 -700 hover:bg-gray-50 -gray-600">
-                
-                <th scope="row" className="flex items-center px-6 py-4 font-medium text-gray-900 whitespace-nowrap ">
-                    <img className="w-10 h-10 rounded-full" src="/docs/images/people/profile-picture-3.jpg" alt="Jese image" />
+                <td className="px-6 py-4">
+                    2
+                </td>
+                <th scope="row" className="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap ">
+                    <img className="w-10 h-10 rounded-full" src="/docs/images/people/profile-picture-1.jpg" alt="Jese image" />
                     <div className="pl-3">
-                        <div className="text-base font-semibold">Bonnie Green</div>
-                        <div className="font-normal text-gray-500">bonnie@flowbite.com</div>
-                    </div>
+                        <div className="text-base font-semibold">Neil Sims</div>
+                        <div className="font-normal text-gray-500">neil.sims@flowbite.com</div>
+                    </div>  
                 </th>
                 <td className="px-6 py-4">
-                    Designer
+                    Neil
+                </td>
+                <td className="px-6 py-4">
+                    Sims
+                </td>
+                <td className="px-6 py-4">
+                    Admin
                 </td>
                 <td className="px-6 py-4">
                     <div className="flex items-center">
@@ -109,20 +127,29 @@ const Users = () => {
                     </div>
                 </td>
                 <td className="px-6 py-4">
-                    <a href="#" className="font-medium text-blue-600 -500 hover:underline">Edit user</a>
+                    {/* <!-- Modal toggle --> */}
+                    <a href="#" type="button" data-modal-target="editUserModal" data-modal-show="editUserModal" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit user</a>
                 </td>
             </tr>
             <tr className="bg-white border-b -800 -700 hover:bg-gray-50 -gray-600">
-                
-                <th scope="row" className="flex items-center px-6 py-4 font-medium text-gray-900 whitespace-nowrap ">
-                    <img className="w-10 h-10 rounded-full" src="/docs/images/people/profile-picture-2.jpg" alt="Jese image" />
+                <td className="px-6 py-4">
+                    3
+                </td>
+                <th scope="row" className="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap ">
+                    <img className="w-10 h-10 rounded-full" src="/docs/images/people/profile-picture-1.jpg" alt="Jese image" />
                     <div className="pl-3">
-                        <div className="text-base font-semibold">Jese Leos</div>
-                        <div className="font-normal text-gray-500">jese@flowbite.com</div>
-                    </div>
+                        <div className="text-base font-semibold">Neil Sims</div>
+                        <div className="font-normal text-gray-500">neil.sims@flowbite.com</div>
+                    </div>  
                 </th>
                 <td className="px-6 py-4">
-                    Vue JS Developer
+                    Neil
+                </td>
+                <td className="px-6 py-4">
+                    Sims
+                </td>
+                <td className="px-6 py-4">
+                    Admin
                 </td>
                 <td className="px-6 py-4">
                     <div className="flex items-center">
@@ -130,20 +157,29 @@ const Users = () => {
                     </div>
                 </td>
                 <td className="px-6 py-4">
-                    <a href="#" className="font-medium text-blue-600 -500 hover:underline">Edit user</a>
+                    {/* <!-- Modal toggle --> */}
+                    <a href="#" type="button" data-modal-target="editUserModal" data-modal-show="editUserModal" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit user</a>
                 </td>
             </tr>
             <tr className="bg-white border-b -800 -700 hover:bg-gray-50 -gray-600">
-                
-                <th scope="row" className="flex items-center px-6 py-4 font-medium text-gray-900 whitespace-nowrap ">
-                    <img className="w-10 h-10 rounded-full" src="/docs/images/people/profile-picture-5.jpg" alt="Jese image" />
+                <td className="px-6 py-4">
+                    4
+                </td>
+                <th scope="row" className="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap ">
+                    <img className="w-10 h-10 rounded-full" src="/docs/images/people/profile-picture-1.jpg" alt="Jese image" />
                     <div className="pl-3">
-                        <div className="text-base font-semibold">Thomas Lean</div>
-                        <div className="font-normal text-gray-500">thomes@flowbite.com</div>
-                    </div>
+                        <div className="text-base font-semibold">Neil Sims</div>
+                        <div className="font-normal text-gray-500">neil.sims@flowbite.com</div>
+                    </div>  
                 </th>
                 <td className="px-6 py-4">
-                    UI/UX Engineer
+                    Neil
+                </td>
+                <td className="px-6 py-4">
+                    Sims
+                </td>
+                <td className="px-6 py-4">
+                    Admin
                 </td>
                 <td className="px-6 py-4">
                     <div className="flex items-center">
@@ -151,31 +187,103 @@ const Users = () => {
                     </div>
                 </td>
                 <td className="px-6 py-4">
-                    <a href="#" className="font-medium text-blue-600 -500 hover:underline">Edit user</a>
+                    {/* <!-- Modal toggle --> */}
+                    <a href="#" type="button" data-modal-target="editUserModal" data-modal-show="editUserModal" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit user</a>
                 </td>
             </tr>
-            <tr className="bg-white -800 hover:bg-gray-50 -gray-600">
-                <th scope="row" className="flex items-center px-6 py-4 font-medium text-gray-900 whitespace-nowrap ">
-                    <img className="w-10 h-10 rounded-full" src="/docs/images/people/profile-picture-4.jpg" alt="Jese image" />
+            <tr className="bg-white border-b -800 -700 hover:bg-gray-50 -gray-600">
+                <td className="px-6 py-4">
+                    5
+                </td>
+                <th scope="row" className="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap ">
+                    <img className="w-10 h-10 rounded-full" src="/docs/images/people/profile-picture-1.jpg" alt="Jese image" />
                     <div className="pl-3">
-                        <div className="text-base font-semibold">Leslie Livingston</div>
-                        <div className="font-normal text-gray-500">leslie@flowbite.com</div>
-                    </div>
+                        <div className="text-base font-semibold">Neil Sims</div>
+                        <div className="font-normal text-gray-500">neil.sims@flowbite.com</div>
+                    </div>  
                 </th>
                 <td className="px-6 py-4">
-                    SEO Specialist
+                    Neil
+                </td>
+                <td className="px-6 py-4">
+                    Sims
+                </td>
+                <td className="px-6 py-4">
+                    Admin
                 </td>
                 <td className="px-6 py-4">
                     <div className="flex items-center">
-                        <div className="h-2.5 w-2.5 rounded-full bg-red-500 mr-2"></div> Offline
+                        <div className="h-2.5 w-2.5 rounded-full bg-green-500 mr-2"></div> Online
                     </div>
                 </td>
                 <td className="px-6 py-4">
-                    <a href="#" className="font-medium text-blue-600 -500 hover:underline">Edit user</a>
+                    {/* <!-- Modal toggle --> */}
+                    <a href="#" type="button" data-modal-target="editUserModal" data-modal-show="editUserModal" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit user</a>
                 </td>
             </tr>
         </tbody>
     </table>
+    {/* <!-- Edit user modal --> */}
+    <div id="editUserModal" tabIndex="-1" aria-hidden="true" className="fixed top-0 left-0 right-0 z-50 items-center justify-center hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+        <div className="relative w-full max-w-2xl max-h-full">
+            {/* <!-- Modal content --> */}
+            <form action="#" className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                {/* <!-- Modal header --> */}
+                <div className="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
+                    <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+                        Edit user
+                    </h3>
+                   <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="editUserModal">
+                    <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                    </svg>
+                    <span className="sr-only">Close modal</span>
+                </button>
+                </div>
+                {/* <!-- Modal body --> */}
+                <div className="p-6 space-y-6">
+                    <div className="grid grid-cols-6 gap-6">
+                        <div className="col-span-6 sm:col-span-3">
+                            <label htmlFor="first-name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First Name</label>
+                            <input type="text" name="first-name" id="first-name" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Bonnie" required=""/>
+                        </div>
+                        <div className="col-span-6 sm:col-span-3">
+                            <label htmlFor="last-name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Last Name</label>
+                            <input type="text" name="last-name" id="last-name" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Green" required=""/>
+                        </div>
+                        <div className="col-span-6 sm:col-span-3">
+                            <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+                            <input type="email" name="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="example@company.com" required=""/>
+                        </div>
+                        <div className="col-span-6 sm:col-span-3">
+                            <label htmlFor="phone-number" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Phone Number</label>
+                            <input type="number" name="phone-number" id="phone-number" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="e.g. +(12)3456 789" required=""/>
+                        </div>
+                        <div className="col-span-6 sm:col-span-3">
+                            <label htmlFor="department" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Department</label>
+                            <input type="text" name="department" id="department" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Development" required=""/>
+                        </div>
+                        <div className="col-span-6 sm:col-span-3">
+                            <label htmlFor="company" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Company</label>
+                            <input type="number" name="company" id="company" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="123456" required=""/>
+                        </div>
+                        <div className="col-span-6 sm:col-span-3">
+                            <label htmlFor="current-password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Current Password</label>
+                            <input type="password" name="current-password" id="current-password" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="••••••••" required=""/>
+                        </div>
+                        <div className="col-span-6 sm:col-span-3">
+                            <label htmlFor="new-password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">New Password</label>
+                            <input type="password" name="new-password" id="new-password" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="••••••••" required=""/>
+                        </div>
+                    </div>
+                </div>
+                    {/* <!-- Modal footer --> */}
+                <div className="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
+                    <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Save all</button>
+                </div>
+            </form>
+        </div>
+    </div>
 </div>
 
     )
