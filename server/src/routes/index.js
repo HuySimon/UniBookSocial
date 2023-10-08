fab0cf883b88cd0686bfb6691761c835a4d7b939
BuiBao3103 <hongbao2003@gmail.com>
2023-10-08 17:43:41 +0700

    feat: get all role

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,9 +1,11 @@
 const AppError = require('../utils/appError')
 const userRouter = require('./userRoutes')
+const roleRoute = require('./roleRoutes')
 
 
 const initRoutes = (app) => {
   app.use('/api/v1/users', userRouter)
+  app.use('/api/v1/roles', roleRoute)
   // app.all('*', (req, res, next) => {
   //   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
   // });
