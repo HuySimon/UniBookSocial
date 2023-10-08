121a4a63e32f350d0614e08816e78a70c08a764f
BuiBao3103 <hongbao2003@gmail.com>
2023-10-08 12:23:11 +0700

    feat: update current user

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -5,13 +5,54 @@ const factory = require('./handlerFactory')
 const db = require('../models');
 const User = db.User
 
-
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach(el => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
 
 exports.getMe = (req, res, next) => {
   req.params.id = req.user.id;
   next();
 };
 
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // 1) Create error if user POSTs password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for password updates. Please use /updateMyPassword.',
+        400
+      )
+    );
+  }
+
+  // 2) Filtered out unwanted fields names that are not allowed to be updated
+  const allowedFields = [
+    'lastName', 'firstName', 'username',
+    'phoneNumber', 'avatar', 'coverImage',
+    'linkFacebook', 'linkZalo', 'linkInstagram'
+  ]
+  const filteredBody = filterObj(req.body, ...allowedFields);
+  console.log(filteredBody)
+  // 3) Update user data
+  const user = await User.update(filteredBody, {
+    where: { id: req.user.id },
+  });
+  if (!user) {
+    return next(new AppError('No user found with that ID', 404));
+  }
+  const userUpdate = await User.findByPk(req.user.id)
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: userUpdate
+    }
+  });
+});
 
 exports.createUser = (req, res) => {
   res.status(500).json({
