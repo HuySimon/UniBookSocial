2be1210c10960ddae2800471dda944acb33ccf84
tikjuti <tangquoctuan2003@gmail.com>
2023-10-08 17:29:01 +0700

    update DeletePost

--- server/src/controllers/postController.js
+++ server/src/controllers/postController.js
@@ -40,8 +40,10 @@ exports.updateStatus = catchAsync(async (req, res, next) => {
 
 exports.isNotDeliveryPost = catchAsync(async (req, res, next) => {
   const post = await Post.findByPk(req.params.id);
-  if (post.userPost !== req.user.id)
-    return next(new AppError("You are not the poster!", 403));
+  if (post.userPost !== req.user.id && req.user.role != 3)
+    return next(
+      new AppError("You do not have permission to delete this post!", 403)
+    );
   if (post.status === "Delivery") {
     return next(new AppError("You can not delete this post!", 403));
   }
