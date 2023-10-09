4ca21f79c197c217d31cb71adfefe19e721d65e6
PhatJack <tienphatng.693@gmail.com>
2023-10-09 15:35:01 +0700

    add Update Current User without validate

--- client/src/views/pages/Profile/ProfileItem/About.jsx
+++ client/src/views/pages/Profile/ProfileItem/About.jsx
@@ -1,138 +1,161 @@
 import React, { useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { useAuthContext } from '../../../../hooks/useAuthContext'
-import * as Yup from 'yup'
 import { yupResolver } from '@hookform/resolvers/yup'
 import Axios from '../../../../api/index'
 import { toast } from 'react-toastify'
 import { useNavigate } from 'react-router-dom'
+import ChangePasswordProfile from './AboutComponents/ChangePasswordProfile'
+import { changeInformationSchema } from '../../../../validations/ProfileValidation'
+
+function isObjectEmpty(obj) {
+	return Object.keys(obj).length === 0;
+}
+
 const About = () => {
 
 	const [currentUser, setCurrentUser] = useState({});
+	const [edit, setEdit] = useState(false)
+	const handleEdit = () => {
+		setEdit(!edit)
+	}
 	const navigate = useNavigate()
 	useEffect(() => {
 		Axios.get('/api/v1/users/me').then(res => {
 			setCurrentUser(res.data.data.data)
-			console.log(res.data.data.data)
 		}).catch(err => {
-			toast.error("Can't get user information")
+			toast.error(err.response.message)
 			navigate('/')
 		})
 	}, [])
 
-	const changePasswordSchema = Yup.object().shape({
-		passwordCurrent: Yup.string().required("Please enter old password").trim(),
-		password: Yup.string().required("Please enter password").matches(
-			/^.*(?=.{8,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/,
-			"Password must contain at least 8 characters, one uppercase, one number and one special case character.Ex(John123@)"
-		),
-		passwordConfirm: Yup.string().required("Please enter confirm password").oneOf([Yup.ref('password')], 'Confirm Password not match!!')
-	})
-
-	const { register, handleSubmit, formState: { errors } } = useForm({
-		defaultValues: {
-			passwordCurrent: "",
-			password: "",
-			passwordConfirm: ""
-		},
-		resolver: yupResolver(changePasswordSchema)
-	})
-	const onSubmitChangePassword = (data) => {
-		const passwords = {
-			passwordCurrent: data.passwordCurrent,
-			password: data.password,
-			passwordConfirm: data.passwordConfirm
-		}
-		Axios.patch('/api/v1/users/updateMyPassword', passwords).then(res => {
-			if (res.status === 200) {
-				toast.success("Change password success!")
-			}
-			console.log(res)
-			console.log(res.data)
-		}).catch(err => {
-			console.log(err.response)
-			toast.error(err.response.data.message)
-		})
+	const preloadValueUser = {
+		firstName: currentUser.firstName,
+		lastName: currentUser.lastName,
+		email: currentUser.email,
+		phoneNumber: currentUser.phoneNumber,
+		linkFacebook: currentUser.linkFacebook,
+		linkInstagram: currentUser.linkInstagram,
+		linkZalo: currentUser.linkZalo
 	}
-	const personalInfor = [
+
+	const { register, handleSubmit, trigger, formState: { errors, dirtyFields, isValid }, getValues } = useForm(
 		{
-			title: "Name",
-			value: (currentUser.firstName + " " + currentUser.lastName)
-		},
-		{
-			title: "Phone",
-			value: currentUser.phoneNumber
-		},
-		{
-			title: "Email",
-			value: currentUser.email
-		},
-		{
-			title: "Facebook",
-			value: (currentUser.linkFacebook === null ? "" : currentUser.linkFacebook)
-		},
-		{
-			title: "Instagram",
-			value: (currentUser.linkInstagram === null ? "" : currentUser.linkInstagram)
-		},
-		{
-			title: "Zalo",
-			value: (currentUser.linkZalo === null ? "" : currentUser.linkZalo)
+			defaultValues: preloadValueUser,
+			resolver: yupResolver(changeInformationSchema),
+		})
+
+	const handleEditInformation = (data) => {
+		const fieldsToTrack = [
+			'firstName',
+			'lastName',
+			'email',
+			'phoneNumber',
+			'linkFacebook',
+			'linkInstagram',
+			'linkZalo',
+		];
+		const curUser = {}
+		fieldsToTrack.forEach((fieldName) => {
+			if (dirtyFields[fieldName]) {
+				curUser[fieldName] = getValues(fieldName)
+				trigger(fieldName)
+			}
+		})
+		console.log(curUser)
+		if (isObjectEmpty(curUser)) {
+			toast.error("You haven't changed anything")	
+		} else {
+			Axios.patch('/api/v1/users/updateMe', curUser).then(res => {
+				if (res.status === 200) {
+					toast.success("Edit information success!")
+				}
+			}).catch(err => {
+				toast.error(err.response.message)
+				console.log(err.response)
+			})
 		}
-	]
+	}
+
 	return (
 		<div className='w-full h-full'>
 			<form
-				action=""
+				onSubmit={handleSubmit(handleEditInformation)}
 				className='w-full flex flex-col gap-5'>
-				{
-					personalInfor.map((item, index) => (
-						<div
-							key={index}
-							className="flex justify-between items-center">
-							<label htmlFor="name" className='w-1/4 font-medium'>{item.title}</label>
-							<input type="text" className='w-3/4 rounded-md px-3 py-2 text-black' value={item.value} />
-						</div>
-					))
-				}
-				<div className="flex justify-between items-start">
-					<label htmlFor="bio" className='w-1/4 font-medium'>Bio</label>
-					<textarea name="" id="" cols="30" rows="7" className='px-3 py-2 border border-[#a0a0a0] rounded-md w-3/4'></textarea>
+				<div className="flex justify-between items-center relative mb-2">
+					<label htmlFor="name" className='w-1/4 font-medium'>First Name</label>
+					<input type="text"
+						disabled={edit ? false : true}
+						{...register("firstName")}
+						defaultValue={currentUser.firstName}
+						className='w-3/4 rounded-md px-3 py-2 text-black' />
+					<p className='text-[12px] text-red-600 absolute top-[46px] left-1/4'>{errors.firstName?.message}</p>
 				</div>
-				<button
-					type="submit"
-					disabled
-					className='w-fit bg-primary-main px-8 py-2 text-white hover:bg-primary-800 cursor-pointer transition-all rounded-md'> Submit
-				</button>
-			</form>
-			<form
-				onSubmit={handleSubmit(onSubmitChangePassword)}
-				className='py-8'>
-				<span className='text-xl'>Change Password</span>
-				<div className="flex flex-col gap-3 mt-4">
-					<div className="flex justify-between items-center">
-						<label htmlFor="" className='w-1/4 font-medium'>Old Password</label>
-						<input type="password" {...register("passwordCurrent")} className='w-3/4 rounded-md px-3 py-2 text-black' />
-					</div>
-					<div className="flex justify-between items-center">
-						<label htmlFor="" className='w-1/4 font-medium'>New Password</label>
-						<input type="password" {...register("password")} className='w-3/4 rounded-md px-3 py-2 text-black' />
-					</div>
-					<div className="flex justify-between items-center">
-						<label htmlFor="" className='w-1/4 font-medium'>Confirm New Password</label>
-						<input type="password" {...register("passwordConfirm")} className='w-3/4 rounded-md px-3 py-2 text-black' />
-					</div>
+				<div className="flex justify-between items-center relative mb-2">
+					<label htmlFor="name" className='w-1/4 font-medium'>Last Name</label>
+					<input type="text"
+						disabled={edit ? false : true}
+						defaultValue={currentUser.lastName}
+						{...register("lastName")} className='w-3/4 rounded-md px-3 py-2 text-black' />
+					<p className='text-[12px] text-red-600 absolute top-[46px] left-1/4'>{errors.lastName?.message}</p>
 				</div>
-				<button
-					type="submit"
-					onClick={() => {
-						toast.error(errors.passwordCurrent?.message)
-						toast.error(errors.password?.message)
-						toast.error(errors.passwordConfirm?.message)
-					}}
-					className='w-fit bg-primary-main px-8 py-2 mt-5 text-white hover:bg-primary-800 cursor-pointer transition-all rounded-md'> Submit
-				</button>
+				<div className="flex justify-between items-center relative mb-2">
+					<label htmlFor="phoneNumber" className='w-1/4 font-medium'>Phone</label>
+					<input type="text"
+						disabled={edit ? false : true}
+						defaultValue={currentUser.phoneNumber}
+						{...register("phoneNumber")} className='w-3/4 rounded-md px-3 py-2 text-black' />
+					<p className='text-[12px] text-red-600 absolute top-[46px] left-1/4'>{errors.phoneNumber?.message}</p>
+				</div>
+				<div className="flex justify-between items-center relative mb-2">
+					<label htmlFor="email" className='w-1/4 font-medium'>Email</label>
+					<input type="text"
+						disabled={edit ? false : true}
+						defaultValue={currentUser.email}
+						{...register("email")} className='w-3/4 rounded-md px-3 py-2 text-black' />
+					<p className='text-[12px] text-red-600 absolute top-[46px] left-1/4'>{errors.email?.message}</p>
+				</div>
+				<div className="flex justify-between items-center relative mb-2">
+					<label htmlFor="linkFacebook" className='w-1/4 font-medium'>Facebook</label>
+					<input type="text"
+						disabled={edit ? false : true}
+						defaultValue={currentUser.linkFacebook}
+						{...register("linkFacebook")} className='w-3/4 rounded-md px-3 py-2 text-black' />
+					<p className='text-[12px] text-red-600 absolute top-[46px] left-1/4'>{errors.linkFacebook?.message}</p>
+				</div>
+				<div className="flex justify-between items-center relative mb-2">
+					<label htmlFor="linkZalo" className='w-1/4 font-medium'>Zalo</label>
+					<input type="text"
+						disabled={edit ? false : true}
+						defaultValue={currentUser.linkZalo}
+						{...register("linkZalo")} className='w-3/4 rounded-md px-3 py-2 text-black' />
+					<p className='text-[12px] text-red-600 absolute top-[46px] left-1/4'>{errors.linkZalo?.message}</p>
+				</div>
+				<div className="flex justify-between items-center relative mb-2">
+					<label htmlFor="linkInstagram" className='w-1/4 font-medium'>Instagram</label>
+					<input type="text"
+						disabled={edit ? false : true}
+						defaultValue={currentUser.linkInstagram}
+						{...register("linkInstagram")} className='w-3/4 rounded-md px-3 py-2 text-black' />
+					<p className='text-[12px] text-red-600 absolute top-[46px] left-1/4'>{errors.linkInstagram?.message}</p>
+				</div>
+				<div className="flex gap-5">
+					<button
+						type='button'
+						onClick={handleEdit}
+						className='w-28 bg-primary-main px-8 py-2 text-white hover:bg-primary-800 cursor-pointer transition-all rounded-md'>
+						{
+							edit ? 'Cancel' : 'Edit'
+						}
+					</button>
+					<button
+						type="submit"
+						disabled={!edit ? true : false}
+						className={`w-28 px-8 py-2 cursor-pointer transition-all rounded-md ${edit ? 'bg-primary-main text-white hover:bg-primary-800 cursor-pointer' : 'bg-gray-200 text-white cursor-not-allowed'}`}> Submit
+					</button>
+				</div>
 			</form>
+			<ChangePasswordProfile />
 		</div>
 	)
 }
--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -6,6 +6,7 @@ const router = express.Router()
 
 router.post('/signup', authController.signup)
 router.post('/login', authController.login)
+router.get('/logout', authController.logout)
 
 router.post('/forgotPassword', authController.forgotPassword)
 router.patch('/resetPassword/:token', authController.resetPassword)
@@ -18,13 +19,13 @@ router.patch('/updateMe', userController.updateMe);
 router.delete('/deleteMe', userController.deleteMe);
 
 router.route('/')
-  .get(userController.getAllUsers)
-  .post(userController.createUser)
+	.get(userController.getAllUsers)
+	.post(userController.createUser)
 
 
 router.route('/:id')
-  .get(userController.getUser)
-  .patch(userController.updateUser)
-  .delete(userController.deleteUser)
+	.get(userController.getUser)
+	.patch(userController.updateUser)
+	.delete(userController.deleteUser)
 
 module.exports = router
