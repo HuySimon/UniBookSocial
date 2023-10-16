63ef3d2dca0ce4c88a7e52f80687cc234f33adfb
AnhHuy <anhhuy2452003@gmail.com>
2023-10-16 14:46:35 +0700

    news fix

--- server/src/controllers/handlerFactory.js
+++ server/src/controllers/handlerFactory.js
@@ -1,71 +1,77 @@
-const querystringParser = require('@bitovi/sequelize-querystring-parser');
-const catchAsync = require('./../utils/catchAsync');
-const AppError = require('./../utils/appError');
+const querystringParser = require("@bitovi/sequelize-querystring-parser");
+const catchAsync = require("./../utils/catchAsync");
+const AppError = require("./../utils/appError");
 
-exports.getAll = Model => catchAsync(async (req, res, next) => {
-  const queryObj = querystringParser.parse(req.url.split('?')[1]);
-  if (queryObj.errors.length) return next(new AppError('Invalid parameter!', 400))
-  const data = await Model.findAll(queryObj.data)
-  res.status(200).json({
-    status: 'success',
-    results: data.length,
-    data: {
-      data
+exports.getAll = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const queryObj = querystringParser.parse(req.url.split("?")[1]);
+    if (queryObj.errors.length)
+      return next(new AppError("Invalid parameter!", 400));
+    const data = await Model.findAndCountAll(queryObj.data);
+    res.status(200).json({
+      status: "success",
+      totalItem: data.count,
+      data: {
+        data: data.rows,
+      },
+    });
+  });
+
+exports.getOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const data = await Model.findByPk(req.params.id);
+    if (!data) {
+      return next(new AppError("No data found with that ID", 404));
     }
+    res.status(200).json({
+      status: "success",
+      data: {
+        data,
+      },
+    });
   });
-})
 
-exports.getOne = Model => catchAsync(async (req, res, next) => {
-  const data = await Model.findByPk(req.params.id);
-  if (!data) {
-    return next(new AppError('No data found with that ID', 404));
-  }
-  res.status(200).json({
-    status: 'success',
-    data: {
-      data
+exports.createOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const data = await Model.create(req.body);
+
+    res.status(201).json({
+      status: "success",
+      data: {
+        data,
+      },
+    });
+  });
+
+exports.updateOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const data = await Model.update(req.body, {
+      where: { id: req.params.id },
+    });
+    if (!data) {
+      return next(new AppError("No data found with that ID", 404));
     }
+    const updateData = await Model.findByPk(req.params.id);
+    res.status(200).json({
+      status: "success",
+      data: {
+        data: updateData,
+      },
+    });
   });
-})
 
-exports.createOne = Model => catchAsync(async (req, res, next) => {
-  const data = await Model.create(req.body);
+exports.deleteOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const data = await Model.destroy({
+      where: { id: req.params.id },
+    });
 
-  res.status(201).json({
-    status: 'success',
-    data: {
-      data
+    if (!data) {
+      return next(new AppError("No data found with that ID", 404));
     }
-  });
-})
 
-exports.updateOne = Model => catchAsync(async (req, res, next) => {
-  const data = await Model.update(req.body, {
-    where: { id: req.params.id },
+    res.status(204).json({
+      status: "success",
+      data: null,
+    });
   });
-  if (!data) {
-    return next(new AppError('No data found with that ID', 404));
-  }
-  const updateData = await Model.findByPk(req.params.id)
-  res.status(200).json({
-    status: 'success',
-    data: {
-      data: updateData
-    }
-  });
-})
-
-exports.deleteOne = Model => catchAsync(async (req, res, next) => {
-  const data = await Model.destroy({
-    where: { id: req.params.id }
-  });
-
-  if (!data) {
-    return next(new AppError('No data found with that ID', 404));
-  }
-
-  res.status(204).json({
-    status: 'success',
-    data: null
-  });
-})
