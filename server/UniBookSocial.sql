1f4b2754e254e824ff50228e1a2f815b677dad93
BuiBao3103 <hongbao2003@gmail.com>
2023-12-10 16:57:18 +0700

    fix: fix database

--- server/UniBookSocial.sql
+++ server/UniBookSocial.sql
@@ -1,10 +1,9 @@
--- Active: 1695112075807@@127.0.0.1@3306@unibooksocial
 -- phpMyAdmin SQL Dump
 -- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Dec 10, 2023 at 08:24 AM
+-- Generation Time: Dec 10, 2023 at 09:56 AM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -45,46 +44,47 @@ CREATE TABLE `notifications` (
 --
 
 INSERT INTO `notifications` (`id`, `isSeen`, `typeNoti`, `content`, `userSend`, `userReceive`, `post`, `createdAt`, `updatedAt`) VALUES
-(1, 1, 'Confirm', NULL, 2, 5, '1', '2023-10-06 07:57:50', '2023-10-06 07:57:50'),
-(2, 0, 'Confirm', NULL, 17, 6, '3', '2023-10-06 08:02:50', '2023-10-06 08:02:50'),
-(3, 0, 'Violation', 'Bài đăng không phù hợp', 10, 20, '2', '2023-10-06 08:13:11', '2023-10-06 08:13:11'),
-(4, 0, 'Unconfirm', NULL, 4, 19, '9', '2023-10-06 08:44:44', '2023-10-06 08:44:44'),
-(5, 1, 'Confirm', NULL, 12, 3, '6', '2023-10-06 08:49:24', '2023-10-06 08:49:24'),
-(6, 0, 'Unconfirm', NULL, 17, 6, '3', '2023-10-06 08:50:31', '2023-10-06 08:50:31'),
-(7, 0, 'CheckPost', NULL, 16, 20, '2', '2023-10-22 21:04:12', '2023-10-22 21:04:12'),
-(8, 0, 'Confirm', NULL, 42, 15, '5', '2023-11-09 21:15:23', '2023-11-09 21:15:23'),
+(1, 1, 'Confirmed', NULL, 2, 5, '1', '2023-10-06 07:57:50', '2023-10-06 07:57:50'),
+(2, 0, 'Confirmed', NULL, 17, 6, '3', '2023-10-06 08:02:50', '2023-10-06 08:02:50'),
+(3, 0, 'Violated', 'Bài đăng không phù hợp', 10, 20, '2', '2023-10-06 08:13:11', '2023-10-06 08:13:11'),
+(4, 0, 'Unconfirmed', NULL, 4, 19, '9', '2023-10-06 08:44:44', '2023-10-06 08:44:44'),
+(5, 1, 'Confirmed', NULL, 12, 3, '6', '2023-10-06 08:49:24', '2023-10-06 08:49:24'),
+(6, 0, 'Unconfirmed', NULL, 17, 6, '3', '2023-10-06 08:50:31', '2023-10-06 08:50:31'),
+(7, 0, 'Checking', NULL, 16, 20, '2', '2023-10-22 21:04:12', '2023-10-22 21:04:12'),
+(8, 0, 'Confirmed', NULL, 42, 15, '5', '2023-11-09 21:15:23', '2023-11-09 21:15:23'),
 (9, 0, 'Unconfirmed', NULL, 42, 15, '5', '2023-11-09 21:17:11', '2023-11-09 21:17:11'),
-(10, 0, 'Confirm', NULL, 42, 15, '5', '2023-11-09 21:18:02', '2023-11-09 21:18:02'),
+(10, 0, 'Confirmed', NULL, 42, 15, '5', '2023-11-09 21:18:02', '2023-11-09 21:18:02'),
 (11, 0, 'Unconfirmed', NULL, 15, 42, '5', '2023-11-09 21:18:59', '2023-11-09 21:18:59'),
-(12, 0, 'CheckPost', 'Nội dung không phù hợphợp', 16, 15, '5', '2023-11-09 21:21:00', '2023-11-09 21:21:00'),
-(13, 0, 'Violation', 'Nội dung không phù hợp', 3, 15, '5', '2023-11-09 21:25:14', '2023-11-09 21:25:14'),
-(14, 0, 'Confirm', NULL, 3, 9, '8', '2023-11-10 18:55:59', '2023-11-10 18:55:59'),
+(12, 0, 'Checking', 'Nội dung không phù hợphợp', 16, 15, '5', '2023-11-09 21:21:00', '2023-11-09 21:21:00'),
+(13, 0, 'Violated', 'Nội dung không phù hợp', 3, 15, '5', '2023-11-09 21:25:14', '2023-11-09 21:25:14'),
+(14, 0, 'Confirmed', NULL, 3, 9, '8', '2023-11-10 18:55:59', '2023-11-10 18:55:59'),
 (15, 0, 'Unconfirmed', NULL, 3, 9, '8', '2023-11-10 18:56:00', '2023-11-10 18:56:00'),
-(16, 0, 'Confirm', NULL, 3, 9, '8', '2023-11-10 18:56:01', '2023-11-10 18:56:01'),
-(17, 0, 'Confirm', NULL, 3, 16, '21', '2023-11-10 18:56:29', '2023-11-10 18:56:29'),
+(16, 0, 'Confirmed', NULL, 3, 9, '8', '2023-11-10 18:56:01', '2023-11-10 18:56:01'),
+(17, 0, 'Confirmed', NULL, 3, 16, '21', '2023-11-10 18:56:29', '2023-11-10 18:56:29'),
 (18, 0, 'Unconfirmed', NULL, 3, 16, '21', '2023-11-10 18:56:32', '2023-11-10 18:56:32'),
-(19, 0, 'Confirm', NULL, 3, 16, '21', '2023-11-10 18:57:01', '2023-11-10 18:57:01'),
-(20, 0, 'Confirm', NULL, 16, 11, '10', '2023-11-10 19:33:24', '2023-11-10 19:33:24'),
+(19, 0, 'Confirmed', NULL, 3, 16, '21', '2023-11-10 18:57:01', '2023-11-10 18:57:01'),
+(20, 0, 'Confirmed', NULL, 16, 11, '10', '2023-11-10 19:33:24', '2023-11-10 19:33:24'),
 (21, 0, 'Unconfirmed', NULL, 16, 11, '10', '2023-11-10 19:33:44', '2023-11-10 19:33:44'),
-(22, 0, 'Confirm', NULL, 16, 11, '10', '2023-11-10 19:33:58', '2023-11-10 19:33:58'),
-(23, 0, 'Confirm', NULL, 41, 16, '23', '2023-11-17 19:59:35', '2023-11-17 19:59:35'),
+(22, 0, 'Confirmed', NULL, 16, 11, '10', '2023-11-10 19:33:58', '2023-11-10 19:33:58'),
+(23, 0, 'Confirmed', NULL, 41, 16, '23', '2023-11-17 19:59:35', '2023-11-17 19:59:35'),
 (24, 0, 'Unconfirmed', NULL, 41, 16, '23', '2023-11-17 20:00:17', '2023-11-17 20:00:17'),
-(25, 0, 'Confirm', NULL, 41, 16, '23', '2023-11-17 20:00:18', '2023-11-17 20:00:18'),
+(25, 0, 'Confirmed', NULL, 41, 16, '23', '2023-11-17 20:00:18', '2023-11-17 20:00:18'),
 (26, 0, 'Unconfirmed', NULL, 41, 16, '23', '2023-11-17 20:00:22', '2023-11-17 20:00:22'),
-(27, 0, 'Confirm', NULL, 41, 16, '23', '2023-11-17 20:00:22', '2023-11-17 20:00:22'),
-(28, 1, 'Confirm', NULL, 6, 16, '22', '2023-11-29 21:23:43', '2023-12-02 19:47:49'),
+(27, 0, 'Confirmed', NULL, 41, 16, '23', '2023-11-17 20:00:22', '2023-11-17 20:00:22'),
+(28, 1, 'Confirmed', NULL, 6, 16, '22', '2023-11-29 21:23:43', '2023-12-02 19:47:49'),
 (29, 1, 'Unconfirmed', NULL, 6, 16, '22', '2023-11-29 21:23:51', '2023-12-02 19:47:19'),
-(30, 1, 'Confirm', NULL, 6, 16, '22', '2023-11-29 21:23:53', '2023-12-02 19:47:14'),
-(31, 0, 'Confirm', NULL, 16, 6, '24', '2023-11-29 21:25:03', '2023-11-29 21:25:03'),
+(30, 1, 'Confirmed', NULL, 6, 16, '22', '2023-11-29 21:23:53', '2023-12-02 19:47:14'),
+(31, 0, 'Confirmed', NULL, 16, 6, '24', '2023-11-29 21:25:03', '2023-11-29 21:25:03'),
 (32, 0, 'Unconfirmed', NULL, 16, 6, '24', '2023-11-29 21:26:52', '2023-11-29 21:26:52'),
-(33, 0, 'Confirm', NULL, 16, 6, '1012230003', '2023-12-10 14:47:24', '2023-12-10 14:47:24'),
-(34, 0, 'Confirm', NULL, 16, 6, '1012230002', '2023-12-10 14:48:57', '2023-12-10 14:48:57'),
-(35, 0, 'CheckPost', NULL, 6, 16, '1012230011', '2023-12-10 14:51:01', '2023-12-10 14:51:01'),
-(36, 0, 'Confirm', NULL, 6, 16, '1012230009', '2023-12-10 14:51:19', '2023-12-10 14:51:19'),
-(37, 0, 'Confirm', NULL, 6, 16, '1012230008', '2023-12-10 14:51:49', '2023-12-10 14:51:49'),
-(38, 0, 'CheckPost', NULL, 16, 6, '1012230012', '2023-12-10 14:56:45', '2023-12-10 14:56:45'),
-(39, 0, 'CheckPost', NULL, 16, 6, '1012230013', '2023-12-10 15:00:40', '2023-12-10 15:00:40'),
-(40, 0, 'Violation', 'Ảnh không phù hợp', 3, 6, '1012230013', '2023-12-10 15:01:10', '2023-12-10 15:01:10');
+(33, 0, 'Confirmed', NULL, 16, 6, '1012230003', '2023-12-10 14:47:24', '2023-12-10 14:47:24'),
+(34, 0, 'Confirmed', NULL, 16, 6, '1012230002', '2023-12-10 14:48:57', '2023-12-10 14:48:57'),
+(35, 0, 'Checking', NULL, 6, 16, '1012230011', '2023-12-10 14:51:01', '2023-12-10 14:51:01'),
+(36, 0, 'Confirmed', NULL, 6, 16, '1012230009', '2023-12-10 14:51:19', '2023-12-10 14:51:19'),
+(37, 0, 'Confirmed', NULL, 6, 16, '1012230008', '2023-12-10 14:51:49', '2023-12-10 14:51:49'),
+(38, 0, 'Checking', NULL, 16, 6, '1012230012', '2023-12-10 14:56:45', '2023-12-10 14:56:45'),
+(39, 0, 'Checking', NULL, 16, 6, '1012230013', '2023-12-10 15:00:40', '2023-12-10 15:00:40'),
+(40, 0, 'Violated', 'Ảnh không phù hợp', 3, 6, '1012230013', '2023-12-10 15:01:10', '2023-12-10 15:01:10'),
+(41, 0, 'Clear', NULL, 3, 6, '1012230012', '2023-12-10 16:46:32', '2023-12-10 16:46:32');
 
 -- --------------------------------------------------------
 
@@ -142,33 +142,33 @@ INSERT INTO `posts` (`id`, `title`, `price`, `mainImage`, `description`, `status
 ('0312230002', 'aef', 4, '1702192297348-871301673.jpg', 'afe', 'Unconfirmed', 1, 0, NULL, 16, '2023-12-03 14:35:24', '2023-12-03 14:35:24'),
 ('0312230003', 'asef', 45, '1702192297348-871301673.jpg', 'aef', 'Unconfirmed', 1, 0, NULL, 16, '2023-12-03 14:35:54', '2023-12-03 14:35:54'),
 ('0312230004', 'asfe', 15000, '1701589067161-712249949.jfif', 'awfe', 'Unconfirmed', 0, 0, NULL, 16, '2023-12-03 14:37:47', '2023-12-03 14:41:59'),
-('1', 'Kinh tế chính trị', 15000, 'mainImage.png', 'Tài liệu tham khảo', 'Confirm', 1, 0, 2, 5, '2023-09-25 02:02:24', '2023-09-25 02:02:24'),
+('1', 'Kinh tế chính trị', 15000, 'mainImage.png', 'Tài liệu tham khảo', 'Confirmed', 1, 0, 2, 5, '2023-09-25 02:02:24', '2023-09-25 02:02:24'),
 ('10', 'Lịch sử Đảng cộng sản Việt Nam', 35000, 'mainImage.png', 'Tài liệu tham khảo', 'Delivered', 1, 0, 16, 11, '2023-09-25 02:02:24', '2023-11-10 19:34:02'),
 ('1012230001', 'Tiếng Anh I', 30000, '1702192297348-871301673.jpg', 'Đã cào code (ghi chép đầy đủ)', 'Unconfirmed', 0, 0, NULL, 6, '2023-12-10 14:11:37', '2023-12-10 14:13:36'),
-('1012230002', 'Tiếng Anh III', 80000, '1702192646657-375441002.jpg', '\r\nBật mí là mấy môn này chị học toàn được A, nên bạn nào mua có khi cũng được A nè ( chị có giải bài tập trong đó khá chi tiết nên là bạn nào phát biểu nhiều thì điểm quá trình sẽ cao lắm đó nha ). Sách hết code\r\n', 'Confirm', 0, 0, 16, 6, '2023-12-10 14:17:26', '2023-12-10 14:48:57'),
+('1012230002', 'Tiếng Anh III', 80000, '1702192646657-375441002.jpg', '\r\nBật mí là mấy môn này chị học toàn được A, nên bạn nào mua có khi cũng được A nè ( chị có giải bài tập trong đó khá chi tiết nên là bạn nào phát biểu nhiều thì điểm quá trình sẽ cao lắm đó nha ). Sách hết code\r\n', 'Confirmed', 0, 0, 16, 6, '2023-12-10 14:17:26', '2023-12-10 14:48:57'),
 ('1012230003', 'Pháp luật đại cương', 20000, '1702192731790-746939778.jpg', 'Bạn nào có nhu cầu thì ib để mình quay clip coi tình trạng sách rõ hơn nhaa. Bìa sách mình hay nghịch vẽ nên v thoi chứ bên trong vẫn okela nha.', 'Delivered', 1, 0, 16, 6, '2023-12-10 14:18:51', '2023-12-10 14:47:56'),
 ('1012230004', 'Luật ngân sách nhà nước', 15000, '1702192884203-134072660.jpg', 'Giáo trình new 99% ít highlight cần pass.', 'Unconfirmed', 1, 1, NULL, 6, '2023-12-10 14:21:24', '2023-12-10 14:21:24'),
 ('1012230005', 'Luật thương mại Việt Nam', 40000, '1702193013477-727518934.jpg', 'GT new 99% ít highlight.', 'Unconfirmed', 1, 1, NULL, 6, '2023-12-10 14:23:33', '2023-12-10 14:23:33'),
 ('1012230006', 'Chủ nghĩa xã hội', 25000, '1702193260803-968656592.jpg', 'Kèm tài liệu giấy mình tìm được trong quá trình học.', 'Unconfirmed', 1, 0, NULL, 16, '2023-12-10 14:27:40', '2023-12-10 14:27:40'),
 ('1012230007', 'Luật hiến pháp Việt Nam', 20000, '1702193516820-640508167.jpg', 'Ngoại hình tà tà vậy, bên trong thì nguyên.', 'Unconfirmed', 0, 1, NULL, 16, '2023-12-10 14:31:56', '2023-12-10 14:31:56'),
-('1012230008', 'Kinh tế vĩ mô', 20000, '1702193849750-628783579.jpg', 'giáo trình+slide bài giảng có ghi chú', 'Confirm', 0, 1, 6, 16, '2023-12-10 14:37:29', '2023-12-10 14:51:49'),
+('1012230008', 'Kinh tế vĩ mô', 20000, '1702193849750-628783579.jpg', 'giáo trình+slide bài giảng có ghi chú', 'Confirmed', 0, 1, 6, 16, '2023-12-10 14:37:29', '2023-12-10 14:51:49'),
 ('1012230009', 'Lịch sử Đảng Cộng sản Việt Nam', 45000, '1702194124331-468474204.jpg', 'sách gốc còn mới 95%, mình có ghi một vài chỗ thôi ạ, giá bìa 90k mình pass 45k', 'Delivered', 1, 0, 6, 16, '2023-12-10 14:42:04', '2023-12-10 14:51:21'),
 ('1012230010', 'Dùng từ viết câu', 15000, '1702194206207-578690322.jpg', 'Cuốn này cho bạn nào học môn tiếng việt thực hành, mình có tặng kèm bài tập.', 'Unconfirmed', 1, 1, NULL, 16, '2023-12-10 14:43:26', '2023-12-10 14:43:26'),
-('1012230011', 'Sách nhập môn khoa học du lịch', 10000, '1702194420631-567309075.jpg', 'Tặng thêm file pdf bài giảng cô Hạnh.', 'CheckPost', 0, 1, NULL, 16, '2023-12-10 14:47:00', '2023-12-10 14:51:01'),
-('1012230012', 'Kinh tế chính trị', 20000, '1702194928934-473670421.jpg', 'Giáo trình mới xài một kì nên mới nha', 'CheckPost', 1, 0, NULL, 6, '2023-12-10 14:55:28', '2023-12-10 14:56:45'),
-('1012230013', 'Tiếng Anh II', 20000, '1702195175373-839439421.jpg', 'Sách mới', 'Violation', 1, 1, NULL, 6, '2023-12-10 14:59:35', '2023-12-10 15:01:10'),
-('2', 'Chủ nghĩa xã hội khoa học', 30000, 'mainImage.png', 'Tài liệu tham khảo', 'Violation', 1, 0, NULL, 20, '2023-09-25 02:02:24', '2023-10-22 20:13:52'),
+('1012230011', 'Sách nhập môn khoa học du lịch', 10000, '1702194420631-567309075.jpg', 'Tặng thêm file pdf bài giảng cô Hạnh.', 'Checking', 0, 1, NULL, 16, '2023-12-10 14:47:00', '2023-12-10 14:51:01'),
+('1012230012', 'Kinh tế chính trị', 20000, '1702194928934-473670421.jpg', 'Giáo trình mới xài một kì nên mới nha', 'Unconfirmed', 1, 0, NULL, 6, '2023-12-10 14:55:28', '2023-12-10 16:46:32'),
+('1012230013', 'Tiếng Anh II', 20000, '1702195175373-839439421.jpg', 'Sách mới', 'Violated', 1, 1, NULL, 6, '2023-12-10 14:59:35', '2023-12-10 15:01:10'),
+('2', 'Chủ nghĩa xã hội khoa học', 30000, 'mainImage.png', 'Tài liệu tham khảo', 'Violated', 1, 0, NULL, 20, '2023-09-25 02:02:24', '2023-10-22 20:13:52'),
 ('21', 'Test2', 150000, '1697789874796-804553370.png', 'oke', 'Delivered', 1, 0, 3, 16, '2023-10-20 15:17:54', '2023-11-10 18:57:02'),
 ('22', 'Hóa 2', 8, '1699620174133-489063823.jfif', 'oke', 'Delivered', 0, 0, 6, 16, '2023-11-10 19:42:54', '2023-11-29 21:23:58'),
 ('23', 'abc', 12, '1699620421278-845035065.jpg', 'dd', 'Delivered', 1, 1, 41, 16, '2023-11-10 19:47:01', '2023-11-17 20:00:24'),
 ('24', 'áef', 34, '1702192297348-871301673.jpg', 'ádfe', 'Unconfirmed', 1, 0, NULL, 6, '2023-11-29 21:24:42', '2023-11-29 21:26:52'),
 ('25', 'aefa', 555, '1702192297348-871301673.jpg', 'áef', 'Unconfirmed', 1, 0, NULL, 16, '2023-11-29 21:31:21', '2023-11-29 21:31:21'),
 ('3', 'Kỹ thuật lập trình', 20000, 'mainImage.png', 'Tài liệu tham khảo', 'Delivery', 1, 1, 17, 6, '2023-09-25 02:02:24', '2023-09-25 02:02:24'),
-('4', 'GDQP và AN I', 10000, 'mainImage.png', 'Tài liệu học tập', 'Confirm', 1, 0, 10, 7, '2023-09-25 02:02:24', '2023-09-25 02:02:24'),
-('5', 'Triết học Mac-Lenin', 25000, 'mainImage.png', 'Tài liệu tham khảo', 'Violation', 0, 0, NULL, 15, '2023-09-25 02:02:24', '2023-11-09 21:25:14'),
+('4', 'GDQP và AN I', 10000, 'mainImage.png', 'Tài liệu học tập', 'Confirmed', 1, 0, 10, 7, '2023-09-25 02:02:24', '2023-09-25 02:02:24'),
+('5', 'Triết học Mac-Lenin', 25000, 'mainImage.png', 'Tài liệu tham khảo', 'Violated', 0, 0, NULL, 15, '2023-09-25 02:02:24', '2023-11-09 21:25:14'),
 ('6', 'Kinh tế vĩ mô', 20000, 'mainImage.png', 'Tài liệu tham khảo', 'Delivery', 1, 1, 12, 3, '2023-09-25 02:02:24', '2023-09-25 02:02:23'),
 ('8', 'Tư tưởng Hồ Chí Minh', 15000, 'mainImage.png', 'Tài liệu tham khảo', 'Delivered', 0, 0, 3, 9, '2023-09-25 02:02:24', '2023-11-10 18:56:02'),
-('9', 'Kinh tế lượng', 15000, 'mainImage.png', 'Tài liệu tham khảo', 'Confirm', 0, 1, 4, 19, '2023-09-25 02:02:24', '2023-09-25 02:02:24');
+('9', 'Kinh tế lượng', 15000, 'mainImage.png', 'Tài liệu tham khảo', 'Confirmed', 0, 1, 4, 19, '2023-09-25 02:02:24', '2023-09-25 02:02:24');
 
 -- --------------------------------------------------------
 
@@ -380,11 +380,9 @@ INSERT INTO `users` (`id`, `email`, `firstName`, `lastName`, `username`, `avatar
 (19, 'tangquoctuan2003@gmail.com', 'Tuấn', 'Tăng', 'Tăng Quốc Tuấn', 'avatarDefault.png', 'coverImageDefault.png', '$2a$12$bYHcLnCYh5lfKKr4D7wmgO1m25mIynEw7nPnx3.SzD67AVOCdhBea', 'Active', 2, NULL, NULL, NULL, NULL, '2023-09-24 22:03:51', '2023-09-24 22:03:51'),
 (20, 'tienphan09098@gmail.com', 'Tiến', 'Phan', 'Tiến Tiến', 'avatarDefault.png', 'coverImageDefault.png', '$2a$12$bYHcLnCYh5lfKKr4D7wmgO1m25mIynEw7nPnx3.SzD67AVOCdhBea', 'Active', 2, NULL, NULL, NULL, NULL, '2023-09-24 22:03:51', '2023-09-24 22:03:51'),
 (21, 'tienphatng.693@gmail.com', 'Phát', 'Nguyễn', 'Phát Nguyễn', 'avatarDefault.png', 'coverImageDefault.png', '$2a$12$bYHcLnCYh5lfKKr4D7wmgO1m25mIynEw7nPnx3.SzD67AVOCdhBea', 'Active', 2, NULL, NULL, NULL, NULL, '2023-09-24 22:03:51', '2023-09-24 22:03:51'),
-(25, 'test3@gmail.com', 'Test', 'Nguyen Van', 'Test Nguyen Van', 'avatarDefault.jpg', 'coverImageDefault.jpg', '$2a$12$bYHcLnCYh5lfKKr4D7wmgO1m25mIynEw7nPnx3.SzD67AVOCdhBea', 'Active', 2, NULL, NULL, NULL, '0908141453', '2023-10-16 18:47:53', '2023-10-16 18:47:53'),
-(41, 'test123@gmail.com', 'Test33333', 'Nguyen Van', 'Test33 Nguyen Van', 'avatarDefault.jpg', '1700224365290-486503796.png', '$2a$12$bYHcLnCYh5lfKKr4D7wmgO1m25mIynEw7nPnx3.SzD67AVOCdhBea', 'Active', 1, NULL, NULL, NULL, '0908141453', '2023-11-05 00:35:18', '2023-11-17 20:06:18'),
-(42, 'testnha@gmail.com', 'Test33', 'Nguyen Van', 'Test33 Nguyen Van', 'avatarDefault.jpg', 'coverImageDefault.jpg', '$2a$12$bYHcLnCYh5lfKKr4D7wmgO1m25mIynEw7nPnx3.SzD67AVOCdhBea', 'Active', 1, NULL, NULL, NULL, '0908141453', '2023-11-07 15:15:44', '2023-11-07 15:15:44'),
-(43, 'hongbao2@gmail.com', 'Bao', 'Bui', 'Bao Bui', 'avatarDefault.jpg', 'coverImageDefault.jpg', '$2a$12$bYHcLnCYh5lfKKr4D7wmgO1m25mIynEw7nPnx3.SzD67AVOCdhBea', 'Active', 1, NULL, NULL, NULL, '0908141453', '2023-11-07 15:17:28', '2023-11-07 15:17:28'),
-(45, 'test1234@gmail.com', 'Test2', 'abc', 'Test2 abc', 'avatarDefault.jpg', 'coverImageDefault.jpg', '$2a$12$bYHcLnCYh5lfKKr4D7wmgO1m25mIynEw7nPnx3.SzD67AVOCdhBea', 'Active', 1, NULL, NULL, NULL, '0908141453', '2023-11-17 20:10:44', '2023-11-17 20:10:44');
+(41, 'test123@gmail.com', 'Test33333', 'Nguyen Van', 'Test33 Nguyen Van', 'avatarDefault.png', '1700224365290-486503796.png', '$2a$12$bYHcLnCYh5lfKKr4D7wmgO1m25mIynEw7nPnx3.SzD67AVOCdhBea', 'Active', 1, NULL, NULL, NULL, '0908141453', '2023-11-05 00:35:18', '2023-11-17 20:06:18'),
+(42, 'testnha@gmail.com', 'Test33', 'Nguyen Van', 'Test33 Nguyen Van', 'avatarDefault.png', 'coverImageDefault.png', '$2a$12$bYHcLnCYh5lfKKr4D7wmgO1m25mIynEw7nPnx3.SzD67AVOCdhBea', 'Active', 1, NULL, NULL, NULL, '0908141453', '2023-11-07 15:15:44', '2023-11-07 15:15:44'),
+(43, 'hongbao2@gmail.com', 'Bao', 'Bui', 'Bao Bui', 'avatarDefault.png', 'coverImageDefault.png', '$2a$12$bYHcLnCYh5lfKKr4D7wmgO1m25mIynEw7nPnx3.SzD67AVOCdhBea', 'Active', 1, NULL, NULL, NULL, '0908141453', '2023-11-07 15:17:28', '2023-11-07 15:17:28');
 
 --
 -- Indexes for dumped tables
@@ -471,7 +469,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `notifications`
 --
 ALTER TABLE `notifications`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
 
 --
 -- AUTO_INCREMENT for table `permissions`
